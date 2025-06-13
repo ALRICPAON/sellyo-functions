baac3a7b73f87a6a954d4aaae2b3bdb9c47cbc94
ALRICPAON <paonalric@gmail.com>
2025-06-13 17:24:02 +0200

    Update firebase-auth-check.js

--- js/firebase-auth-check.js
+++ js/firebase-auth-check.js
@@ -1,19 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+// Imports Firebase
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
-// Configuration Firebase (✅ BUCKET CORRIGÉ ICI)
-const firebaseConfig = {
-  apiKey: "AIzaSyC2yzKA3kESPjgcFk6pojJQK4rNToywqJI",
-  authDomain: "sellyo-3bbdb.firebaseapp.com",
-  projectId: "sellyo-3bbdb",
-  storageBucket: "sellyo-3bbdb.firebasestorage.app", // ✅ CORRECT
-  messagingSenderId: "465249279278",
-  appId: "1:465249279278:web:319844f7477ab47930eebf"
-};
+// Import config Firebase DEV
+import { app } from './firebase-config-dev.js';
 
-// Initialisation Firebase
-const app = initializeApp(firebaseConfig);
+// Initialisation des services Firebase
 const auth = getAuth(app);
 const db = getFirestore(app);
 
