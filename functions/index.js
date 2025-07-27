e70ad84f942974f756e1000ba983ed8a32a42f4d
ALRICPAON <paonalric@gmail.com>
2025-07-27 14:13:41 +0200

    🚀 Ajout fonction Firebase sécurisée createMailerSendDomain pour MailerSend

--- functions/index.js
+++ functions/index.js
@@ -3,11 +3,11 @@ const { checkScheduledEmails } = require("./checkScheduledEmails");
 const { handleNewLeadWorkflow } = require("./handleNewLeadWorkflow");
 const { modifyEmail } = require("./modifyEmail");
 const { submitMainWebhook } = require("./submitMainWebhook");
-
+const { createMailerSendDomain } = require("./createMailerSendDomain");
 
 exports.submitMainWebhook = submitMainWebhook;
 exports.sendEmailOnReady = sendEmailOnReady;
 exports.checkScheduledEmails = checkScheduledEmails;
 exports.handleNewLeadWorkflow = handleNewLeadWorkflow;
-exports.modifyEmail = require("./modifyEmail").modifyEmail;
-
+exports.modifyEmail = modifyEmail; // 👈 pas besoin de require à nouveau ici
+exports.createMailerSendDomain = createMailerSendDomain;
