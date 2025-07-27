3d4627405ccd5759511cde826d89259c9d5ad7cc
ALRICPAON <paonalric@gmail.com>
2025-07-27 11:30:10 +0200

    🚀 MAJ submit-email.js avec appel à submitMainWebhook

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,10 @@ const { sendEmailOnReady } = require("./sendEmailOnReady");
 const { checkScheduledEmails } = require("./checkScheduledEmails");
 const { handleNewLeadWorkflow } = require("./handleNewLeadWorkflow");
 const { modifyEmail } = require("./modifyEmail");
+const { submitMainWebhook } = require("./submitMainWebhook");
 
+
+exports.submitMainWebhook = submitMainWebhook;
 exports.sendEmailOnReady = sendEmailOnReady;
 exports.checkScheduledEmails = checkScheduledEmails;
 exports.handleNewLeadWorkflow = handleNewLeadWorkflow;
